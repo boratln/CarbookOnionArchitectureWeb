322ed0fc70cfe1af4ba1707609c7c2b52a0640af
boratln <boratulan123@gmail.com>
2024-09-24 17:58:19 +0300

    WebUI Default View Components

---
+++ Carbook.WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUIComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Dto.BannerDtos;
+using Carbook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Carbook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCoverUIComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultCoverUIComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Carbook.WebUI/ViewComponents/DefaultViewComponents/_DefaultLast5CarWithBrandComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Carbook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultLast5CarWithBrandComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLast5CarWithBrandComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Cars/GetLast5Cars");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultLast5CarWithBrandDto>> (jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
