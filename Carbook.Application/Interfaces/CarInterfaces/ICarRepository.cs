a2e8ee60c31129f74a7f693ba5a81ebd7c52c29d
boratln <boratulan123@gmail.com>
2024-09-24 17:51:26 +0300

    IRepositories

---
+++ Carbook.Application/Interfaces/CarInterfaces/ICarRepository.cs
@@ -0,0 +1,15 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces.CarInterfaces
+{
+    public interface ICarRepository
+    {
+       Task <List<Car>> GetCarsListWithBrand();
+        Task<List<Car>> GetLast5CarsWithBrand();
+    }
+}
---
+++ Carbook.Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces
+{
+    public interface IRepository<T> where T:class
+    {
+        Task<List<T>> GetAll();
+        Task <T> GetById(int id);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
