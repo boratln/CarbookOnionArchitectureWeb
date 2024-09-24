c54929c9687a4838bddc6390e498be05efbfcdf1
boratln <boratulan123@gmail.com>
2024-09-24 17:32:38 +0300

    Pricing Mediator Handlers

---
+++ Carbook.Application/Features/Mediator/Handlers/PricingHandlers/GetPricingByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Carbook.Application.Features.Mediator.Queries.PricingQueries;
+using Carbook.Application.Features.Mediator.Results.LocationResults;
+using Carbook.Application.Features.Mediator.Results.PricingResults;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.PricingHandlers
+{
+    public class GetPricingByIdQueryHandler : IRequestHandler<GetPricingByIdQuery,GetPricingByIdQueryResult>
+    {
+        private readonly IRepository<Pricing> _repository;
+
+        public GetPricingByIdQueryHandler(IRepository<Pricing> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetPricingByIdQueryResult> Handle(GetPricingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetById(request.Id);
+            return new GetPricingByIdQueryResult
+            {
+               PricingId=value.PricingId,
+                Name = value.Name
+            };
+        }
+
+       
+    }
+}
---
+++ Carbook.Application/Features/Mediator/Handlers/PricingHandlers/UpdatePricingCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Carbook.Application.Features.Mediator.Commands.PricingCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.PricingHandlers
+{
+    public class UpdatePricingCommandHandler : IRequestHandler<UpdatePricingCommand>
+    {
+        private readonly IRepository<Pricing> _repository;
+
+        public UpdatePricingCommandHandler(IRepository<Pricing> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdatePricingCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetById(request.PricingId);
+            value.Name = request.Name;
+
+            await _repository.Update(value);
+        }
+    }
+}
