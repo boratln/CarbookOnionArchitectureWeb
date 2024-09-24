e674811b2f88847b1aa68484a770eda724f8126b
boratln <boratulan123@gmail.com>
2024-09-24 17:52:51 +0300

    Service Registiration

---
+++ Carbook.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddApplicationService(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
+        }
+    }
+
+}
