71e0767f06e5df5e10196b1b51fe0fbadceb6382
boratln <boratulan123@gmail.com>
2024-09-24 17:37:09 +0300

    Feature Mediator Queries

---
+++ Carbook.Application/Features/Mediator/Queries/FeatureQueries/GetFeatureByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.FeatureResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.FeatureQueries
+{
+    public class GetFeatureByIdQuery:IRequest<GetFeatureByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetFeatureByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
