fab7c67952266b7eb1bb53f43c5c617801120a00
boratln <boratulan123@gmail.com>
2024-09-24 17:29:46 +0300

    Service Mediator Commands

---
+++ Carbook.Application/Features/Mediator/Commands/ServicesCommands/RemoveServicesCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Commands.ServicesCommands
+{
+    public class RemoveServicesCommand:IRequest
+    {
+        public int Id { get; set; }
+
+        public RemoveServicesCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
