760c5b2ca548df9a4e2a9ef8be01b28a2840e5f4
boratln <boratulan123@gmail.com>
2024-09-24 17:19:16 +0300

    Car CQRS Results

---
+++ Carbook.Application/Features/CQRS/Results/CarResults/GetCarQueryResult.cs
@@ -0,0 +1,24 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.CQRS.Results.CarResults
+{
+    public class GetCarQueryResult
+    {
+        public int CarId { get; set; }
+        public string CarModel { get; set; }
+
+        public int BrandId { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Kilometer { get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+    }
+}
