106d92d4c46c87cd2b3b0abd1639e8b58d5effb7
boratln <boratulan123@gmail.com>
2024-09-24 17:36:56 +0300

    Testimonial Mediator Handlers

---
+++ Carbook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Carbook.Application.Features.Mediator.Queries.TestimonialQueries;
+using Carbook.Application.Features.Mediator.Results.TestimonialResults;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class GetTestimonialQueryHandler : IRequestHandler<GetTestimonialQuery, List<GetTestimonialQueryResult>>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetTestimonialQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetTestimonialQueryResult>> Handle(GetTestimonialQuery request, CancellationToken cancellationToken)
+        {
+           var values=await _repository.GetAll();
+            return values.Select(x => new GetTestimonialQueryResult
+            {
+                Comment = x.Comment,
+                ImageUrl = x.ImageUrl,
+                Name = x.Name,
+                TestimonialId = x.TestimonialId,
+                Title = x.Title,
+            }).ToList();
+        }
+    }
+}
---
+++ Carbook.Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Commands.TestimonialCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class UpdateTestimonialHandler : IRequestHandler<UpdateTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public UpdateTestimonialHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+           var value=await _repository.GetById(request.TestimonialId);
+            value.Title = request.Title;
+            value.Comment = request.Comment;
+            value.ImageUrl = request.ImageUrl;
+            await _repository.Update(value);
+        }
+    }
+}
