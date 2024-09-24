904ce6110fbfa8cd48f2c4441e1e507ef45a844a
boratln <boratulan123@gmail.com>
2024-09-24 17:18:41 +0300

    Brand CQRS Results

---
+++ Carbook.Application/Features/CQRS/Results/BrandResults/GetBrandQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.CQRS.Results.BrandResults
+{
+    public class GetBrandQueryResult
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
