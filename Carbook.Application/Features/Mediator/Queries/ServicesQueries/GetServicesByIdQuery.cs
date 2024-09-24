f3dc248175d998011c7e3c819c345e1bb880fa82
boratln <boratulan123@gmail.com>
2024-09-24 17:38:04 +0300

    Services Mediator Queries

---
+++ Carbook.Application/Features/Mediator/Queries/ServicesQueries/GetServicesByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.ServicesResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.ServicesQueries
+{
+    public class GetServicesByIdQuery:IRequest<GetServicesByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetServicesByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
---
+++ Carbook.Application/Features/Mediator/Queries/ServicesQueries/GetServicesQuery.cs
@@ -0,0 +1,14 @@
+﻿using Carbook.Application.Features.Mediator.Results.ServicesResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.ServicesQueries
+{
+    public class GetServicesQuery:IRequest<List<GetServiceQueryResult>>
+    {
+    }
+}
