f20278d7cd8fab3077872805b06afbe8dda381e2
boratln <boratulan123@gmail.com>
2024-09-25 18:21:44 +0300

    WebUI Blog Dtos

---
+++ Carbook.Dto/BlogDtos/ResultLast3BlogDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Dto.BlogDtos
+{
+    public class ResultLast3BlogDto
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
