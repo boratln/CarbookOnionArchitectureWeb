c632600ee52614d859ed1c2f5b627a2d125143b1
boratln <boratulan123@gmail.com>
2024-09-25 18:20:09 +0300

    Author Mediator Handlers

---
+++ Carbook.Application/Features/Mediator/Handlers/AuthorHandlers/GetAutohrByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Carbook.Application.Features.Mediator.Queries.AuthorQueries;
+using Carbook.Application.Features.Mediator.Results.AuthorResults;
+using Carbook.Application.Features.Mediator.Results.FeatureResults;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.AuthorHandlers
+{
+    public class GetAutohrByIdQueryHandler : IRequestHandler<GetAuthorByIdQuery, GetAuthorByIdQueryResult>
+    {
+        private readonly IRepository<Author> _repository;
+
+        public GetAutohrByIdQueryHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAuthorByIdQueryResult> Handle(GetAuthorByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetById(request.Id);
+            return new GetAuthorByIdQueryResult
+            {
+                AuthorId = request.Id,
+                Description=value.Description,
+                ImageUrl=value.ImageUrl,            
+                Name = value.Name
+            };
+        }
+    }
+}
---
+++ Carbook.Application/Features/Mediator/Handlers/AuthorHandlers/UpdateAuthorCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Carbook.Application.Features.Mediator.Commands.AuthorCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.AuthorHandlers
+{
+    public class UpdateAuthorCommandHandler : IRequestHandler<UpdateAuthorCommand>
+    {
+        private readonly IRepository<Author> _repository;
+
+        public UpdateAuthorCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetById(request.AuthorId);
+            value.Description = request.Description;
+            value.ImageUrl = request.ImageUrl;
+
+            value.Name = request.Name;
+
+            await _repository.Update(value);
+        }
+    }
+}
