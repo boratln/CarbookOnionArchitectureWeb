c94e0c2821224e54e94305dbbc38e68354acc6be
boratln <boratulan123@gmail.com>
2024-09-25 18:39:53 +0300

    Blog Mediator Results

---
+++ Carbook.Application/Features/Mediator/Results/BlogResults/GetLast3BlogWithAuthorQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Results.BlogResults
+{
+    public class GetLast3BlogWithAuthorQueryResult
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
