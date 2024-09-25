182979194e6982a708873d66a4e5ef00e382d87f
boratln <boratulan123@gmail.com>
2024-09-25 18:40:24 +0300

    Default Page Blog Component Partial

---
+++ Carbook.WebUI/ViewComponents/DefaultViewComponents/_DefaultBlogComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Carbook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultBlogComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultBlogComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7076/api/Blogs/GetLast3BlogWithAuthor");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData=await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultLast3BlogDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
