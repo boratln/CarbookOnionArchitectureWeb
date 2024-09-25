e501de1facaea993c1077e2db84ede3ee300370f
boratln <boratulan123@gmail.com>
2024-09-25 18:22:00 +0300

    BlogRepository

---
+++ Carbook.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Interfaces.BlogInterFaces;
+using Carbook.Domain.Entities;
+using Carbook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories.BlogRepositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly CarbookContext _carbookContext;
+
+        public BlogRepository(CarbookContext carbookContext)
+        {
+            _carbookContext = carbookContext;
+        }
+
+        public List<Blog> GetLast3BlogWithAuthors()
+        {
+            var values = _carbookContext.Blogs.Include(x => x.Author).OrderByDescending(x => x.BlogId).Take(3).ToList();
+            return values;
+        }
+    }
+}
