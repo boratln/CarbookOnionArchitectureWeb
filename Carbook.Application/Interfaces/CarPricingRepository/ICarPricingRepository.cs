c50650ae59b7835dcbcdb9e8fa535885ad53ed8d
boratln <boratulan123@gmail.com>
2024-09-28 22:06:20 +0300

    Car Pricing Repository Interface

---
+++ Carbook.Application/Interfaces/CarPricingRepository/ICarPricingRepository.cs
@@ -0,0 +1,15 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Repositories.CarPricingRepository
+{
+	public interface ICarPricingRepository
+	{
+		Task<List<CarPricing>> GetCarPricingWithCars();
+
+	}
+}
