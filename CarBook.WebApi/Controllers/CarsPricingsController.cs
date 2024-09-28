0ac7dd9de840d16eb2701e05034370ae4547a420
boratln <boratulan123@gmail.com>
2024-09-28 22:07:51 +0300

    CarsPricings Api Controller

---
+++ CarBook.WebApi/Controllers/CarsPricingsController.cs
@@ -0,0 +1,25 @@
+﻿using Carbook.Application.Features.Mediator.Queries.CarPricingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CarsPricingsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CarsPricingsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		[HttpGet]
+		public async  Task<IActionResult> GetAllCarPricings()
+		{
+			var values=await _mediator.Send(new GetCarPricingQuery());
+			return Ok(values);
+		}
+	}
+}
