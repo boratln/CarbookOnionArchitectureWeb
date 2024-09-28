63323e8cf69fc8bd53b0bfb862b6bac36133851d
boratln <boratulan123@gmail.com>
2024-09-28 22:06:48 +0300

    WebUI GetAllCarPricingDto

---
+++ Carbook.Dto/CarPricingDtos/ResultCarPricingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Dto.CarPricingDtos
+{
+	public class ResultCarPricingDto
+	{
+        public int CarPricingId { get; set; }
+        public string PricingName { get; set; }
+        public string BrandName { get; set; }
+        public string CarModel { get; set; }
+        public decimal Price { get; set; }
+        public string CoverImageUrl { get; set; }
+    }
+}
