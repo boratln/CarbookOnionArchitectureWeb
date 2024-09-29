d189f7582b49c2303b1670237f6b60f4d7932181
boratln <boratulan123@gmail.com>
2024-09-29 13:55:43 +0300

    Mediator Update Blog Command

--- Carbook.Application/Features/Mediator/Commands/BlogCommands/UpdateBlogCommand.cs
+++ Carbook.Application/Features/Mediator/Commands/BlogCommands/UpdateBlogCommand.cs
@@ -11,6 +11,8 @@ namespace Carbook.Application.Features.Mediator.Commands.BlogCommands
     {
         public int BlogId { get; set; }
         public string Title { get; set; }
+        public string BlogDescription { get; set; }
+
         public int AuthorId { get; set; }
         public string CoverImageUrl { get; set; }
         public DateTime CreatedDate { get; set; }
