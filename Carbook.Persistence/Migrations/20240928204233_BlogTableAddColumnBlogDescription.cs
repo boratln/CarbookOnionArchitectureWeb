6fc5cd516b59d7cfb4c6bfa91dae6b6e834afbe4
boratln <boratulan123@gmail.com>
2024-09-29 14:13:27 +0300

    Blog Table Add Column Blog Description Migration

---
+++ Carbook.Persistence/Migrations/20240928204233_BlogTableAddColumnBlogDescription.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carbook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogTableAddColumnBlogDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+          name: "BlogDescription",          
+          table: "Blogs",         
+          type: "nvarchar(max)",   
+          nullable: true);         
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+       name: "BlogDescription",
+       table: "Blogs");
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+         
+        }
+    }
+}
