654fb79487efa39bf3979711b31abb4aed77088b
boratln <boratulan123@gmail.com>
2024-09-29 14:15:50 +0300

    WebUI Blog Controller Add Index Method

---
+++ Carbook.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Carbook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.WebSockets;
+
+namespace Carbook.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            TempData["title"] = "Bloglar";
+            TempData["desc"] = "Bloglarımız";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Blogs/GetAllBlogWithAuthor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
