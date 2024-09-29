14be62bd39ff3775d5c3a6f65d19fdcf82b1428e
boratln <boratulan123@gmail.com>
2024-09-29 13:58:11 +0300

    Mediator GetAllBlogWithAuthor Handler

---
+++ Carbook.Application/Features/Mediator/Handlers/BlogHandlers/GetAllBlogsWithAuthorQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Carbook.Application.Features.Mediator.Queries.BlogQueries;
+using Carbook.Application.Features.Mediator.Results.BlogResults;
+using Carbook.Application.Interfaces;
+using Carbook.Application.Interfaces.BlogInterFaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetAllBlogsWithAuthorQueryHandler : IRequestHandler<GetBlogsWithAuthorQuery, List<GetAllBlogsWithAuthorQueryResult>>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetAllBlogsWithAuthorQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async  Task<List<GetAllBlogsWithAuthorQueryResult>> Handle(GetBlogsWithAuthorQuery request, CancellationToken cancellationToken)
+        {
+            var values =  _repository.GetAllBlogsWithAuthors();
+            return values.Select(x => new GetAllBlogsWithAuthorQueryResult
+            {
+                AuthorId = x.AuthorId,
+                AuthorName=x.Author.Name,
+                BlogId=x.BlogId,
+                CategoryId=x.CategoryId,
+                CategoryName=x.Category.Name,
+                CoverImageUrl=x.CoverImageUrl,
+                CreatedDate=x.CreatedDate,
+                BlogDescription=x.BlogDescription,
+                Title=x.Title
+
+            }).ToList();
+        }
+    }
+}
