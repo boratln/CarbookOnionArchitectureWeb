efa3480b46356d6c4cc7da7b7f6b7ab0221ada37
boratln <boratulan123@gmail.com>
2024-09-30 16:34:09 +0300

    Create Tag Cloud Api Controller

---
+++ CarBook.WebApi/Controllers/TagCloudsController.cs
@@ -0,0 +1,52 @@
+﻿using Carbook.Application.Features.Mediator.Commands.TagCloudCommands;
+using Carbook.Application.Features.Mediator.Queries.TagCloudQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagCloudsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagCloudsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var values = await _mediator.Send(new GetTagCloudQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await _mediator.Send(new GetTagCloudByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateTagCloudCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Etiket Bulutu başarıyla eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateTagCloudCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Etiket Bulutu başarıyla güncellendi");
+
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new RemoveTagCloudCommand(id));
+            return Ok("Etiket Bulutu başarıyla silindi");
+        }
+    }
+}
