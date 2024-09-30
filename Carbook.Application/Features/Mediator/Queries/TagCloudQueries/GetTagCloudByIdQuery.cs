5307f6255dfe62083fd13349e0a73468a82cfb8e
boratln <boratulan123@gmail.com>
2024-09-30 15:38:08 +0300

    Mediator Create GetTagCloudByIdQuery

---
+++ Carbook.Application/Features/Mediator/Queries/TagCloudQueries/GetTagCloudByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.TagCloudResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.TagCloudQueries
+{
+    public class GetTagCloudByIdQuery:IRequest<GetTagCloudByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetTagCloudByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
