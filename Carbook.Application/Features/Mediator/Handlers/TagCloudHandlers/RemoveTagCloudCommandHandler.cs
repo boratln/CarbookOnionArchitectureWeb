ccdd8f60e24b578977705d8f52f41a0511c1f4da
boratln <boratulan123@gmail.com>
2024-09-30 15:37:16 +0300

    Mediator Create RemoveTagCloudCommandHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/TagCloudHandlers/RemoveTagCloudCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Features.Mediator.Commands.TagCloudCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class RemoveTagCloudCommandHandler : IRequestHandler<RemoveTagCloudCommand>
+    {
+        private readonly IRepository<TagCloud> _repository;
+
+        public RemoveTagCloudCommandHandler(IRepository<TagCloud> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveTagCloudCommand request, CancellationToken cancellationToken)
+        {
+            TagCloud cloud = await _repository.GetById(request.Id);
+            await _repository.Remove(cloud);
+        }
+    }
+}
