11c7defd20ea86809faac5b1dfa1b42500ec8669
boratln <boratulan123@gmail.com>
2024-09-30 15:36:23 +0300

    Mediator CreateTagCloudCommandHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/TagCloudHandlers/CreateTagCloudCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Commands.TagCloudCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class CreateTagCloudCommandHandler : IRequestHandler<CreateTagCloudCommand>
+    {
+        private readonly IRepository<TagCloud> _repository;
+
+        public CreateTagCloudCommandHandler(IRepository<TagCloud> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateTagCloudCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.Create(new TagCloud
+            {
+                BlogId = request.BlogId,
+                Title = request.Title,
+            });
+        }
+    }
+}
