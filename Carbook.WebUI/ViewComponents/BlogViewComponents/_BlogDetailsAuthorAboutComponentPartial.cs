f1c84bc7f4c76153c8daf9188d58102c993527d3
boratln <boratulan123@gmail.com>
2024-10-01 15:15:54 +0300

    Update BlogDetailAuthorAboutComponentPartial

--- Carbook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
+++ Carbook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsAuthorAboutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsAuthorAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Blogs/GetByBlogWithAuthor/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var getbyauthordto = new GetByBlogAndAuthorDto();
+                getbyauthordto.getByBlogWithAuthorDto = JsonConvert.DeserializeObject<GetByBlogWithAuthorDto>(jsonData);
+                return View(getbyauthordto);
+            }
             return View();
         }
     }
