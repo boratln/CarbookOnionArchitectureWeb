e9d2bed6c18f36d89b7034f71ab41662f60b458a
boratln <boratulan123@gmail.com>
2024-10-01 15:23:07 +0300

    Create GetByBlogWithAuthorQuery

---
+++ Carbook.Application/Features/Mediator/Queries/BlogQueries/GetByBlogWithAuthorQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class GetByBlogWithAuthorQuery:IRequest<GetByBlogWithAuthorQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetByBlogWithAuthorQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
