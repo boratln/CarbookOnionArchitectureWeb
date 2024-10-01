983a2f09ef9411fbc767a8d32702fe37e0ffb290
boratln <boratulan123@gmail.com>
2024-10-01 15:20:46 +0300

    Create Tag Cloud Interface

---
+++ Carbook.Application/Interfaces/TagCloudInterfaces/ITagCloudRepository.cs
@@ -0,0 +1,14 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces.TagCloudInterfaces
+{
+    public interface ITagCloudRepository
+    {
+        List<TagCloud> GetTagCloudByBlogId(int id);
+    }
+}
