6006937303f0332d34d87ca32ba54a4a9cc891c3
boratln <boratulan123@gmail.com>
2024-10-01 15:28:17 +0300

    Web UI Create GetByBlogWithAuthorDto

---
+++ Carbook.Dto/BlogDtos/GetByBlogWithAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Dto.BlogDtos
+{
+    public class GetByBlogWithAuthorDto
+    {
+        public int BlogId { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
