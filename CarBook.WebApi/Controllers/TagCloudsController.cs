9dcc471d6fd8108616e8e98f8878c8399431b722
boratln <boratulan123@gmail.com>
2024-10-01 15:38:25 +0300

    Update TagCloudsController Add GetTagCloudByBlogId Method

--- CarBook.WebApi/Controllers/TagCloudsController.cs
+++ CarBook.WebApi/Controllers/TagCloudsController.cs
@@ -48,5 +48,11 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(new RemoveTagCloudCommand(id));
             return Ok("Etiket Bulutu başarıyla silindi");
         }
+        [HttpGet("GetTagCloudByBlogId/{id}")]
+        public async Task<IActionResult> GetTagCloudByBlogId(int id)
+        {
+            var values=await _mediator.Send(new GetTagCloudByBlogIdQuery(id));
+            return Ok(values);
+        }
     }
 }
