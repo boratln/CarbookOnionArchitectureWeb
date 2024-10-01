9138b2f5ea9f00105b728242ffb08612c9f03269
boratln <boratulan123@gmail.com>
2024-10-01 15:21:31 +0300

    Create GetByBlogWithAuthorQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/BlogHandlers/GetByBlogWithAuthorQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Carbook.Application.Features.Mediator.Queries.BlogQueries;
+using Carbook.Application.Features.Mediator.Results.BlogResults;
+using Carbook.Application.Interfaces.BlogInterFaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetByBlogWithAuthorQueryHandler : IRequestHandler<GetByBlogWithAuthorQuery, GetByBlogWithAuthorQueryResult>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetByBlogWithAuthorQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetByBlogWithAuthorQueryResult> Handle(GetByBlogWithAuthorQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetByBlogWithAuthor(request.Id);
+            return new GetByBlogWithAuthorQueryResult
+            {
+                BlogId = request.Id,
+                AuthorId = value.AuthorId,
+                AuthorName = value.Author.Name,
+                AuthorUrl = value.Author.ImageUrl
+            };
+        }
+    }
+}
