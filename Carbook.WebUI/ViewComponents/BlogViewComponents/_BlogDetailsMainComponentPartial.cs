dcdef483c084124cc13e986b6a44be8ccbd83672
boratln <boratulan123@gmail.com>
2024-10-01 15:04:33 +0300

    Edit BlogDetailMainComponentPartial

--- Carbook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsMainComponentPartial.cs
+++ Carbook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsMainComponentPartial.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsMainComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsMainComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Blogs/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var getbyblogdto = new GetByBlogAndAuthorDto();
+                getbyblogdto.getByBlogDto = JsonConvert.DeserializeObject<GetByBlogDto>(jsonData);
+
+                
+                return View(getbyblogdto);
+
+            }
             return View();
         }
     }
