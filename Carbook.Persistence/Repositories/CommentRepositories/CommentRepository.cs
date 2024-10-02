e81385ffdd0a98bb1119eac63ae2f8276fa3869d
boratln <boratulan123@gmail.com>
2024-10-02 14:54:55 +0300

    Create CommentRepository

---
+++ Carbook.Persistence/Repositories/CommentRepositories/CommentRepository.cs
@@ -0,0 +1,56 @@
+﻿using Carbook.Application.Features.RepositoryPattern.CommentRepositories;
+using Carbook.Domain.Entities;
+using Carbook.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories.CommentRepositories
+{
+    public class CommentRepository<T> : IGenericRepository<Comment>
+    {
+        private readonly CarbookContext _context;
+
+        public CommentRepository(CarbookContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Comment entity)
+        {
+           _context.Comments.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comments.Select(x => new Comment
+            {
+                BlogId = x.BlogId,
+                CommentId = x.CommentId,
+                CommentText = x.CommentText,
+                CreatedDate = x.CreatedDate,
+                Name = x.Name,
+            }).ToList();
+        }
+
+        public Comment GetById(int id)
+        {
+            return _context.Comments.FirstOrDefault(x => x.CommentId == id);
+        }
+
+        public void Remove(Comment entity)
+        {
+            _context.Comments.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(Comment entity)
+        {
+            _context.Comments.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
