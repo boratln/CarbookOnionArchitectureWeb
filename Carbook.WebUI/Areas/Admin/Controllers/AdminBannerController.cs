5054965e5c7d02472155d1e37b77eee0425a2343
boratln <boratulan123@gmail.com>
2024-10-10 18:56:51 +0300

    WebUI Admin Banner Controller

---
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,108 @@
+﻿using Carbook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminBanner")]
+    public class AdminBannerController : Controller
+    {
+        
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task< IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateBanner")]
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateBanner")]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBannerDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Banners", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new {area="Admin"});
+            }
+            return View();
+
+        }
+        [Route("DeleteBanner/{id}")]
+
+        public async Task<IActionResult> DeleteBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Banners/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        [Route("UpdateBanner/{id}")]
+        public async Task<IActionResult> UpdateBanner(int id)
+        {
+            HttpContext.Session.SetInt32("bannerid", id);
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Banners/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateBannerDto>(jsonData);
+
+                return View(value);
+            }
+            return View(new UpdateBannerDto());
+        }
+        [HttpPost]
+        [Route("UpdateBanner/{id}")]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            int id =(int) HttpContext.Session.GetInt32("bannerid");
+
+            updateBannerDto.BannerId = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBannerDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Banners", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Remove("bannerid");
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+
+        }
+    }
+}
