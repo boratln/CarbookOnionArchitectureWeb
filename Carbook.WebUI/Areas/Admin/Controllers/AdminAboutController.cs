b185627192ecd84a62201f9fb12f568427e6d253
boratln <boratulan123@gmail.com>
2024-10-10 18:56:37 +0300

    WebUI Admin About Controller

---
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
@@ -0,0 +1,108 @@
+﻿using Carbook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminAbout")]
+    public class AdminAboutController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateAbout")]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateAbout")]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAboutDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Abouts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+            }
+            return View();
+
+        }
+        [Route("DeleteAbout/{id}")]
+
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Abouts/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        [Route("UpdateAbout/{id}")]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            HttpContext.Session.SetInt32("Aboutid", id);
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Abouts/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+
+                return View(value);
+            }
+            return View(new UpdateAboutDto());
+        }
+        [HttpPost]
+        [Route("UpdateAbout/{id}")]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            int id = (int)HttpContext.Session.GetInt32("Aboutid");
+
+            updateAboutDto.AboutId = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Abouts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Remove("Aboutid");
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+
+        }
+    }
+}
