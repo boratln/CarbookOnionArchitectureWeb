2824d03d97a983541b80ab3a504ec564e63d7636
boratln <boratulan123@gmail.com>
2024-10-11 13:47:49 +0300

    WebUI AdminCategoryController

---
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,107 @@
+﻿using Carbook.Dto.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminCategory")]
+    public class AdminCategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Categorys");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Categorys", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
+            }
+            return View();
+
+        }
+        [Route("DeleteCategory/{id}")]
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Categorys/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        [Route("UpdateCategory/{id}")]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            HttpContext.Session.SetInt32("Categoryid", id);
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Categorys/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+
+                return View(value);
+            }
+            return View(new UpdateCategoryDto());
+        }
+        [HttpPost]
+        [Route("UpdateCategory/{id}")]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            int id = (int)HttpContext.Session.GetInt32("Categoryid");
+
+            updateCategoryDto.CategoryId = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Categorys", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Remove("Categoryid");
+                return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
+
+        }
+    }
+}
