876a8488a7345fc1c5f0c7def8979f169ed8ef9b
boratln <boratulan123@gmail.com>
2024-10-11 13:47:38 +0300

    WebUI AdminAuthorController

---
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminAuthorController.cs
@@ -0,0 +1,107 @@
+﻿using Carbook.Dto.AuthorDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminAuthor")]
+    public class AdminAuthorController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAuthorController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Authors");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateAuthor")]
+        public IActionResult CreateAuthor()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateAuthor")]
+        public async Task<IActionResult> CreateAuthor(CreateAuthorDto createAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAuthorDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Authors", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+            }
+            return View();
+
+        }
+        [Route("DeleteAuthor/{id}")]
+
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Authors/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        [Route("UpdateAuthor/{id}")]
+        public async Task<IActionResult> UpdateAuthor(int id)
+        {
+            HttpContext.Session.SetInt32("Authorid", id);
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Authors/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateAuthorDto>(jsonData);
+
+                return View(value);
+            }
+            return View(new UpdateAuthorDto());
+        }
+        [HttpPost]
+        [Route("UpdateAuthor/{id}")]
+        public async Task<IActionResult> UpdateAuthor(UpdateAuthorDto updateAuthorDto)
+        {
+            int id = (int)HttpContext.Session.GetInt32("Authorid");
+
+            updateAuthorDto.AuthorId = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAuthorDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Authors", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Remove("Authorid");
+                return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminAuthor", new { area = "Admin" });
+
+        }
+    }
+}
