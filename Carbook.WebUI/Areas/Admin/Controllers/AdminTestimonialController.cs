10d6465817c7039cc995ef45a82bcb660254a37f
boratln <boratulan123@gmail.com>
2024-10-21 22:34:02 +0300

    Web UI Create Admin Testimonial Controller

---
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminTestimonialController.cs
@@ -0,0 +1,107 @@
+﻿using Carbook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminTestimonial")]
+    public class AdminTestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminTestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateTestimonial")]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateTestimonial")]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createTestimonialDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Testimonials", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminTestimonial", new { area = "Admin" });
+            }
+            return View();
+
+        }
+        [Route("DeleteTestimonial/{id}")]
+
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Testimonials/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminTestimonial", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminTestimonial", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        [Route("UpdateTestimonial/{id}")]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            HttpContext.Session.SetInt32("Testimonialid", id);
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Testimonials/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateTestimonialDto>(jsonData);
+
+                return View(value);
+            }
+            return View(new UpdateTestimonialDto());
+        }
+        [HttpPost]
+        [Route("UpdateTestimonial/{id}")]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            int id = (int)HttpContext.Session.GetInt32("Testimonialid");
+
+            updateTestimonialDto.TestimonialId = id;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateTestimonialDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Testimonials", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Remove("Testimonialid");
+                return RedirectToAction("Index", "AdminTestimonial", new { area = "Admin" });
+
+            }
+            return RedirectToAction("Index", "AdminTestimonial", new { area = "Admin" });
+
+        }
+    }
+}
