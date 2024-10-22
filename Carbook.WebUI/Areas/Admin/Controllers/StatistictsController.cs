d5e9fad7546263b3dafbafd3cff3deca5cc282e2
boratln <boratulan123@gmail.com>
2024-10-22 23:13:21 +0300

    Web UI Statisticts Controller

---
+++ Carbook.WebUI/Areas/Admin/Controllers/StatistictsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminStatisticts")]
+    public class AdminStatistictsController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminStatistictsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+}
