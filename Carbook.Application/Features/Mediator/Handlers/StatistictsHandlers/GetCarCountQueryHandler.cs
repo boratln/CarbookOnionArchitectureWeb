c9efccea3aae21df93eb9afe7d278cc2aa68a9bb
boratln <boratulan123@gmail.com>
2024-10-22 23:09:30 +0300

    Mediator GetCarCountQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetCarCountQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces;
+using Carbook.Application.Interfaces.CarInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetCarCountQueryHandler : IRequestHandler<GetCarCountQuery, GetCarCountQueryResult>
+    {
+        private readonly ICarRepository _carRepository;
+
+        public GetCarCountQueryHandler(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async Task<GetCarCountQueryResult> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            var value = _carRepository.GetCarCount();
+            return new GetCarCountQueryResult
+            {
+                CarCount = value,
+            };
+        }
+    }
+}
