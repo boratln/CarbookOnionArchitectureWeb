88a78d5ed58e023f74a5836c5b17c1d992649390
boratln <boratulan123@gmail.com>
2024-10-22 23:12:59 +0300

    Web Api Statisticts Controller

---
+++ CarBook.WebApi/Controllers/StatistictsController.cs
@@ -0,0 +1,25 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatistictsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StatistictsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("GetCarCount")]
+        public async Task<IActionResult> GetCarCount()
+        {
+            var count = await _mediator.Send(new GetCarCountQuery());
+            return Ok(count);
+        }
+    }
+}
