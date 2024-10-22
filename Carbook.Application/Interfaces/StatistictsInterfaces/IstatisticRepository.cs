5ca2576b446c7c78e6d6668f17ea8edf65bd1e0e
boratln <boratulan123@gmail.com>
2024-10-22 23:11:31 +0300

    Statistic Repository Interface

---
+++ Carbook.Application/Interfaces/StatistictsInterfaces/IstatisticRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces.StatistictsInterfaces
+{
+    public interface IstatisticRepository
+    {
+         int GetCarCount();
+         int GetAuthorCount();
+         int GetBlogCount();
+         int GetBrandCount();
+        double GetAvgRentPriceForDaily();
+        double GetAvgRentPriceForWeekly();
+        double GetAvgRentPriceForMonthly();
+        int GetCarCountByTransmissionIsAuto();
+        string BrandNameByMaxCar();
+        string BlogTitleByMaxBlogComment();
+        int GetCarCountByKmSmallerThan1000();
+        int GetCarCountByFuelGasolineOrDiesel();
+        int GetCarCountByFuelElectric();
+        string GetCarBrandAndModelByRentPriceDailyMax();
+        string GetCarBrandAndModelByRentPriceDailyMin();
+    }
+}
