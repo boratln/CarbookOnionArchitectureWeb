3a2938f045943a52bed33f74a9a9f5194a684849
boratln <boratulan123@gmail.com>
2024-11-03 19:14:12 +0300

    Mediator GetBrandNameByMaxCarQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetBrandNameByMaxCarQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetBrandNameByMaxCarQueryHandler : IRequestHandler<GetBrandNameByMaxCarQuery, GetBrandNameByMaxCarQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetBrandNameByMaxCarQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetBrandNameByMaxCarQueryResult> Handle(GetBrandNameByMaxCarQuery request, CancellationToken cancellationToken)
+        {
+            var GetBrandNameByMaxCar = _statisticrepository.GetBrandNameByMaxCar();
+            return new GetBrandNameByMaxCarQueryResult { GetBrandNameByMaxCar = GetBrandNameByMaxCar };
+        }
+    }
+}
