abedc6eeadd0a316f9489147dcf8caed45b0500c
boratln <boratulan123@gmail.com>
2024-11-03 19:31:50 +0300

    Mediator GetAvgRentPriceForWeeklyQuery

---
+++ Carbook.Application/Features/Mediator/Queries/StatistictsQueries/GetAvgRentPriceForWeeklyQuery.cs
@@ -0,0 +1,14 @@
+﻿using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.StatistictsQueries
+{
+    public class GetAvgRentPriceForWeeklyQuery:IRequest<GetAvgRentPriceForWeeklyQueryResult>
+    {
+    }
+}
