5eaa383875cd9a95913122db91db6c67a88a1716
boratln <boratulan123@gmail.com>
2024-11-03 19:11:06 +0300

    Mediator GetAvgRentPriceForDailyQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetAvgRentPriceForDailyQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetAvgRentPriceForDailyQueryHandler : IRequestHandler<GetAvgRentPriceForDailyQuery, GetAvgRentPriceForDailyQueryResult>
+    {
+        private readonly IstatisticRepository statisticRepository;
+
+        public GetAvgRentPriceForDailyQueryHandler(IstatisticRepository statisticRepository)
+        {
+            this.statisticRepository = statisticRepository;
+        }
+
+        public async Task<GetAvgRentPriceForDailyQueryResult> Handle(GetAvgRentPriceForDailyQuery request, CancellationToken cancellationToken)
+        {
+            var avgrentprice = statisticRepository.GetAvgRentPriceForDaily();
+            return new GetAvgRentPriceForDailyQueryResult
+            {
+                avgRentPriceForDaily = avgrentprice
+            };
+        }
+    }
+}
