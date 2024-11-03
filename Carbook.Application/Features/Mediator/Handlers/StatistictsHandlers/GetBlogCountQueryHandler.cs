052fe560f2445431a8b4e12b3b4e757984c299f4
boratln <boratulan123@gmail.com>
2024-11-03 19:13:16 +0300

    Mediator GetBlogCountQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetBlogCountQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetBlogCountQueryHandler : IRequestHandler<GetBlogCountQuery, GetBlogCountQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetBlogCountQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetBlogCountQueryResult> Handle(GetBlogCountQuery request, CancellationToken cancellationToken)
+        {
+            var blogCount = _statisticrepository.GetBlogCount();
+            return new GetBlogCountQueryResult { BlogCount = blogCount };
+        }
+    }
+}
