5373da9ebde78657932046e88cabf5b5b7881f83
boratln <boratulan123@gmail.com>
2024-11-03 19:10:50 +0300

    Mediator GetAuthorCountQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetAuthorCountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetAuthorCountQueryHandler : IRequestHandler<GetAuthorCountQuery, GetAuthorCountQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetAuthorCountQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetAuthorCountQueryResult> Handle(GetAuthorCountQuery request, CancellationToken cancellationToken)
+        {
+           var authorCount=_statisticrepository.GetAuthorCount();
+            return new GetAuthorCountQueryResult
+            {
+                AuthorCount=authorCount
+            };
+        }
+    }
+}
