deabc8f58d560af3d8c7681aef5fe6f6f1f9774d
boratln <boratulan123@gmail.com>
2024-11-03 19:13:35 +0300

    Mediator GetBlogTitleByMaxBlogCommentQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetBlogTitleByMaxBlogCommentQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetBlogTitleByMaxBlogCommentQueryHandler : IRequestHandler<GetBlogTitleByMaxBlogCommentQuery, GetBlogTitleByMaxBlogCommentQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetBlogTitleByMaxBlogCommentQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetBlogTitleByMaxBlogCommentQueryResult> Handle(GetBlogTitleByMaxBlogCommentQuery request, CancellationToken cancellationToken)
+        {
+            string blogName = _statisticrepository.GetBlogTitleByMaxBlogComment();
+            return new GetBlogTitleByMaxBlogCommentQueryResult { BlogName = blogName };
+        }
+    }
+}
