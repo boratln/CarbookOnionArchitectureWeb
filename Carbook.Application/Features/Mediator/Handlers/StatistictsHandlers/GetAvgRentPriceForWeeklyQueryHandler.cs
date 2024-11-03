f8e308f3261ee0b53fdb174878c69316ce3e82b9
boratln <boratulan123@gmail.com>
2024-11-03 19:12:50 +0300

    Mediatorr GetAvgRentPriceForWeeklyQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetAvgRentPriceForWeeklyQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetAvgRentPriceForWeeklyQueryHandler : IRequestHandler<GetAvgRentPriceForWeeklyQuery, GetAvgRentPriceForWeeklyQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetAvgRentPriceForWeeklyQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetAvgRentPriceForWeeklyQueryResult> Handle(GetAvgRentPriceForWeeklyQuery request, CancellationToken cancellationToken)
+        {
+            var GetAvgRentPriceForWeekly = _statisticrepository.GetAvgRentPriceForWeekly();
+            return new GetAvgRentPriceForWeeklyQueryResult
+            {
+                    avgRentPriceForWeekly = GetAvgRentPriceForWeekly
+            };
+        }
+    }
+}
