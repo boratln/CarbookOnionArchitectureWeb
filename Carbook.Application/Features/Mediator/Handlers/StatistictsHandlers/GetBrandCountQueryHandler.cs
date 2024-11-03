964b829acedb9848ca56dd2083dee8fd460f1091
boratln <boratulan123@gmail.com>
2024-11-03 19:13:52 +0300

    Mediator GetBrandCountQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/StatistictsHandlers/GetBrandCountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.StatistictsQueries;
+using Carbook.Application.Features.Mediator.Results.StatistictsResults;
+using Carbook.Application.Interfaces.StatistictsInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.StatistictsHandlers
+{
+    public class GetBrandCountQueryHandler : IRequestHandler<GetBrandCountQuery, GetBrandCountQueryResult>
+    {
+        private readonly IstatisticRepository _statisticrepository;
+
+        public GetBrandCountQueryHandler(IstatisticRepository statisticrepository)
+        {
+            _statisticrepository = statisticrepository;
+        }
+
+        public async Task<GetBrandCountQueryResult> Handle(GetBrandCountQuery request, CancellationToken cancellationToken)
+        {
+            var BrandCount = _statisticrepository.GetBrandCount();
+            return new GetBrandCountQueryResult
+            {
+                BrandCount = BrandCount,
+            };
+        }
+    }
+}
