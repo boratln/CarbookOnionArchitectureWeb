380bba91ea1cf94f3f04c9b74869c110c7875944
boratln <boratulan123@gmail.com>
2024-11-03 19:38:56 +0300

    Mediator GetCarCountByKmSmallerThen1000QueryResult

---
+++ Carbook.Application/Features/Mediator/Results/StatistictsResults/GetCarCountByKmSmallerThen1000QueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Results.StatistictsResults
+{
+    public class GetCarCountByKmSmallerThen1000QueryResult
+    {
+        public int GetCarCountByKmSmallerThen1000 {  get; set; }
+    }
+}
