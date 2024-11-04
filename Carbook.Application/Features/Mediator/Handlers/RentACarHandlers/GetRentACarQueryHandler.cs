abdf896c8ae8a33c9e5c53a849c0f26dbad3bc40
boratln <boratulan123@gmail.com>
2024-11-04 21:40:11 +0300

    Mediator GetRentACarQueryHandler

---
+++ Carbook.Application/Features/Mediator/Handlers/RentACarHandlers/GetRentACarQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Carbook.Application.Features.Mediator.Queries.RentACarQueries;
+using Carbook.Application.Features.Mediator.Results.RentACarResults;
+using Carbook.Application.Interfaces.RentACarInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.RentACarHandlers
+{
+    public class GetRentACarQueryHandler : IRequestHandler<GetRentACarQuery, List<GetRentACarQueryResult>>
+    {
+        private readonly IRentACarRepository _rentacarepository;
+
+        public GetRentACarQueryHandler(IRentACarRepository rentacarepository)
+        {
+            _rentacarepository = rentacarepository;
+        }
+
+        public async Task<List<GetRentACarQueryResult>> Handle(GetRentACarQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _rentacarepository.GetByFilter(x => x.LocationId == request.LocationId && x.Avaliable == true);
+            var results = values.Select(y => new GetRentACarQueryResult
+            {
+                CarId = y.CarId,
+                BrandName = y.Car.Brand.Name,
+                CarModel = y.Car.CarModel,
+                CoverImageUrl = y.Car.CoverImageUrl,
+
+            }).ToList();       
+                return results;
+        }
+    }
+}
