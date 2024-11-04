d05a5493962f0c160bd255ec386dd84cf307a869
boratln <boratulan123@gmail.com>
2024-11-04 21:43:25 +0300

    RentACar Repository

---
+++ Carbook.Persistence/Repositories/RentACarRepositories/RentACarRepository.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Results.RentACarResults;
+using Carbook.Application.Interfaces.RentACarInterfaces;
+using Carbook.Domain.Entities;
+using Carbook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories.RentACarRepositories
+{
+    public class RentACarRepository : IRentACarRepository
+    {
+        private readonly CarbookContext _context;
+
+        public RentACarRepository(CarbookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task <List<RentACar>> GetByFilter(Expression<Func<RentACar, bool>> filter)
+        {
+            var values = await _context.RentACars.Where(filter).Include(x => x.Car).ThenInclude(y => y.Brand).ToListAsync();
+         
+            return values;
+        }
+    }
+}
