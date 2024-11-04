078a8ef96e056b9172fa6b0c6814d5a743da3ec5
boratln <boratulan123@gmail.com>
2024-11-04 21:40:34 +0300

    Mediator GetRentACarQueryResult

---
+++ Carbook.Application/Features/Mediator/Results/RentACarResults/GetRentACarQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Results.RentACarResults
+{
+    public class GetRentACarQueryResult
+    {
+		public int CarId { get; set; }
+		public string BrandName { get; set; }
+		public string CarModel { get; set; }
+		public decimal Price { get; set; }
+		public string CoverImageUrl { get; set; }
+	}
+}
