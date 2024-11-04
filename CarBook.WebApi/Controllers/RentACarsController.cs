1cca44ad82a5a581f40dc8431e9afe2c46b7e5d4
boratln <boratulan123@gmail.com>
2024-11-04 21:43:48 +0300

    Rent A Car Api Controller

---
+++ CarBook.WebApi/Controllers/RentACarsController.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Features.Mediator.Queries.RentACarQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentACarsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RentACarsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("{LocationId}/{Avaliable}")]
+         public async Task<IActionResult> GetRentACarListByLocation(int LocationId,bool Avaliable)
+        {
+            GetRentACarQuery query=new GetRentACarQuery();
+            query.LocationId = LocationId;
+            query.Avaliable = Avaliable;
+            var values = await _mediator.Send(query);
+            return Ok(values);
+        }
+    }
+}
