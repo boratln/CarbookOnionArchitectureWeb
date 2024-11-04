2906d56edfd068e4c982bc2d870fddf1811698b6
boratln <boratulan123@gmail.com>
2024-11-04 21:59:15 +0300

    Web UI Rent A Car List Filter View Component

---
+++ Carbook.WebUI/ViewComponents/RentACarFilterComponents/_RentACarListFilterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.ViewComponents.RentACarFilterComponents
+{
+    public class _RentACarListFilterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke(string v)
+        {
+            TempData["value"] = v;
+            return View();
+        }
+    }
+}
