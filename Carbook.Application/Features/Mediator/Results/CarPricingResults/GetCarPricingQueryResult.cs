250f57319672cfd53add51e8a3c4f3a0831e9fa4
boratln <boratulan123@gmail.com>
2024-11-05 21:32:44 +0300

    Add Property

--- Carbook.Application/Features/Mediator/Results/CarPricingResults/GetCarPricingQueryResult.cs
+++ Carbook.Application/Features/Mediator/Results/CarPricingResults/GetCarPricingQueryResult.cs
@@ -9,6 +9,7 @@ namespace Carbook.Application.Features.Mediator.Results.CarPricingResults
 	public class GetCarPricingQueryResult
 	{
 		public int CarPricingId {  get; set; }
+		public int CarId {  get; set; }
 		public string PricingName { get; set; }
 		public string BrandName {  get; set; }
 		public string CarModel {  get; set; }
