d4eb22f795c5dc5c798e9c8b89fe6c72d6498047
boratln <boratulan123@gmail.com>
2024-11-05 21:40:11 +0300

    Web Api Reservations Controller

---
+++ CarBook.WebApi/Controllers/ReservationsController.cs
@@ -0,0 +1,25 @@
+﻿using Carbook.Application.Features.Mediator.Commands.ReservationCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ReservationsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation(CreateReservationCommand command)
+        {
+            await mediator.Send(command);
+            return Ok("Rezervasyon oluşturuldu");
+        }
+    }
+}
