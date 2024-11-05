04035676bf680085392ca96d9e08367259398a0c
boratln <boratulan123@gmail.com>
2024-11-05 21:40:39 +0300

    Web UI Car Pricing Controller

---
+++ Carbook.WebUI/Controllers/CarPricingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.Controllers
+{
+    public class CarPricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["title"] = "Paketler";
+            TempData["desc"] = "Araç Fiyat Paketleri";
+            return View();
+        }
+    }
+}
