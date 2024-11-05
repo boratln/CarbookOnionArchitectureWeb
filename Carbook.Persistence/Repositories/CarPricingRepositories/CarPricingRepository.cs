a92f44adf5c2aa8f8bb0c17e5bf9ef992d25b553
boratln <boratulan123@gmail.com>
2024-11-05 21:39:53 +0300

    Update CarPricingRepository.cs

--- Carbook.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ Carbook.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Carbook.Application.Repositories.CarPricingRepository;
+using Carbook.Application.ViewModels;
 using Carbook.Domain.Entities;
 using Carbook.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,71 @@ namespace Carbook.Persistence.Repositories.CarPricingRepositories
 			
 			var values = _context.CarPricings.Include(x => x.Car).ThenInclude(y => y.Brand).Include(x => x.Pricing).Where(x=>x.PricingId==3).ToList();
 			return values;
+		}
+
+		public List<CarPricingViewModel> GetCarPricingWithTimePeriod()
+		{
+			List<CarPricingViewModel> values = new List<CarPricingViewModel>();
+			using (var command = _context.Database.GetDbConnection().CreateCommand())
+			{
+				command.CommandText = "Select * From (Select CarModel,Name,CoverImageUrl,PricingId,Price From CarPricings Inner Join Cars On Cars.CarId=CarPricings.CarId Inner Join Brands On Brands.BrandId=Cars.BrandId) As SourceTable Pivot (Sum(Price) For PricingId In ([3],[4],[5])) as PivotTable;";
+				command.CommandType = System.Data.CommandType.Text;
+				_context.Database.OpenConnection();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						CarPricingViewModel carPricingViewModel = new CarPricingViewModel()
+						{
+							Brand = reader["Name"].ToString(),
+							Model = reader["CarModel"].ToString(),
+							CoverImageUrl = reader["CoverImageUrl"].ToString(),
+							Amounts = new List<decimal>
+					{
+						reader["3"] != DBNull.Value ? Convert.ToDecimal(reader["3"]) : 0,
+						reader["4"] != DBNull.Value ? Convert.ToDecimal(reader["4"]) : 0,
+						reader["5"] != DBNull.Value ? Convert.ToDecimal(reader["5"]) : 0
+					}
+						};
+						values.Add(carPricingViewModel);
+					}
+				}
+				_context.Database.CloseConnection();
+				return values;
+			}
+		}
+
+
+		public List<CarPricingViewModel> GetCarPricingWithTimePeriod1()
+		{
+			List<CarPricingViewModel> values = new List<CarPricingViewModel>();
+			using (var command = _context.Database.GetDbConnection().CreateCommand())
+			{
+				command.CommandText = "Select * From (Select CarModel,Name,CoverImageUrl,PricingId,Price From CarPricings Inner Join Cars On Cars.CarId=CarPricings.CarId Inner Join Brands On Brands.BrandId=Cars.BrandId) As SourceTable Pivot (Sum(Price) For PricingId In ([3],[4],[5])) as PivotTable;";
+				command.CommandType = System.Data.CommandType.Text;
+				_context.Database.OpenConnection();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						CarPricingViewModel carPricingViewModel = new CarPricingViewModel()
+						{
+							Brand = reader["Name"].ToString(),
+							Model = reader["CarModel"].ToString(),
+							CoverImageUrl = reader["CoverImageUrl"].ToString(),
+							Amounts = new List<decimal>
+							{
+								Convert.ToDecimal(reader["3"]),
+								Convert.ToDecimal(reader["4"]),
+								Convert.ToDecimal(reader["5"])
+							}
+						};
+						values.Add(carPricingViewModel);
+					}
+				}
+				_context.Database.CloseConnection();
+				return values;
+			}
 		}
 	}
 }
