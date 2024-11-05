77edf3f1c25d53ca00c4d2735e38460d9acaf0f6
boratln <boratulan123@gmail.com>
2024-11-05 21:37:15 +0300

    Add GetCarPricingWithTimePeriod interface method

--- Carbook.Application/Interfaces/CarPricingInterfaces/ICarPricingRepository.cs
+++ Carbook.Application/Interfaces/CarPricingInterfaces/ICarPricingRepository.cs
@@ -1,4 +1,5 @@
-﻿using Carbook.Domain.Entities;
+﻿using Carbook.Application.ViewModels;
+using Carbook.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace Carbook.Application.Repositories.CarPricingRepository
 	public interface ICarPricingRepository
 	{
 		Task<List<CarPricing>> GetCarPricingWithCars();
+		List<CarPricingViewModel> GetCarPricingWithTimePeriod();
 
 	}
 }
