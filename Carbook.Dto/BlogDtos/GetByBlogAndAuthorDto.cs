dc642295edd5528c965d5a8a64c0c1a7bdf79f52
boratln <boratulan123@gmail.com>
2024-11-06 20:53:44 +0300

    Update GetByBlogAndAuthorDto.cs

--- Carbook.Dto/BlogDtos/GetByBlogAndAuthorDto.cs
+++ Carbook.Dto/BlogDtos/GetByBlogAndAuthorDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Carbook.Dto.CommentDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace Carbook.Dto.BlogDtos
     {
         public GetByBlogDto getByBlogDto { get; set; }
         public GetByBlogWithAuthorDto getByBlogWithAuthorDto { get; set; }
+        public CreateCommentDto createCommentDto { get; set; }
     }
 }
