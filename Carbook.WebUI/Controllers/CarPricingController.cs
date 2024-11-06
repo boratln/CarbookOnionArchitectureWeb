4e7d3b8d4b29ac36b385ef97c80b4adb16eb3541
boratln <boratulan123@gmail.com>
2024-11-06 20:56:17 +0300

    Update CarPricingController.cs

--- Carbook.WebUI/Controllers/CarPricingController.cs
+++ Carbook.WebUI/Controllers/CarPricingController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public CarPricingController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async  Task<IActionResult> Index()
         {
             TempData["title"] = "Paketler";
             TempData["desc"] = "Araç Fiyat Paketleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/CarsPricings/GetCarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
