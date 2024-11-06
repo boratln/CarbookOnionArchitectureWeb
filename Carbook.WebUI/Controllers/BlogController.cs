80a0636cc17721c366cebc453c70cf9f6083ef25
boratln <boratulan123@gmail.com>
2024-11-06 20:56:12 +0300

    Update BlogController.cs

--- Carbook.WebUI/Controllers/BlogController.cs
+++ Carbook.WebUI/Controllers/BlogController.cs
@@ -1,7 +1,10 @@
 ﻿using Carbook.Dto.BlogDtos;
+using Carbook.Dto.CommentDtos;
+using Carbook.Dto.LocationDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.WebSockets;
+using System.Text;
 
 namespace Carbook.WebUI.Controllers
 {
@@ -33,17 +36,45 @@ namespace Carbook.WebUI.Controllers
             TempData["title"] = "Bloglar";
             TempData["desc"] = "Blog Detayı ve Yorumlar";
             ViewBag.blogid = id;
-            //var client = _httpClientFactory.CreateClient();
-            //var responseMessage = await client.GetAsync("https://localhost:7076/api/Blogs/"+id);
-            //if (responseMessage.IsSuccessStatusCode)
-            //{
-            //    var jsonData= await responseMessage.Content.ReadAsStringAsync();
-            //    var value = JsonConvert.DeserializeObject<GetByBlogDto>(jsonData);
-            //    return View(value);
-            //}
+            HttpContext.Session.SetInt32("id", id);
+   
             var dto = new GetByBlogAndAuthorDto();
 
             return View(dto);
+        }
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            var id = HttpContext.Session.GetInt32("id");
+            var viewmodel = new GetByBlogAndAuthorDto
+            {
+                createCommentDto = new CreateCommentDto(),
+                getByBlogDto = new GetByBlogDto(),
+                getByBlogWithAuthorDto = new GetByBlogWithAuthorDto(),
+            };
+            return PartialView(viewmodel);
+        }
+        [HttpPost]
+        public async  Task<IActionResult> AddComment(string name,string message,string id,string email)
+        {
+            var blogid = int.Parse(id);
+
+            CreateCommentDto commentDto = new CreateCommentDto();
+            commentDto.CommentText = message;
+            commentDto.Name= name;
+            commentDto.Email= email;
+            commentDto.BlogId = blogid;
+            commentDto.CreatedDate= DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(commentDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Comments/AddComment", content);
+            var data=await responseMessage.Content.ReadAsStringAsync();
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View();
         }
     }
 }
