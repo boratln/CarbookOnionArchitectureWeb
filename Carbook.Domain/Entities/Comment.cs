bb17c5057fa87a9a81598e79e56b8055a0b1ccc8
boratln <boratulan123@gmail.com>
2024-11-06 20:53:36 +0300

    Update Comment.cs

--- Carbook.Domain/Entities/Comment.cs
+++ Carbook.Domain/Entities/Comment.cs
@@ -9,6 +9,7 @@ namespace Carbook.Domain.Entities
     public class Comment
     { public int CommentId {  get; set; }
         public string Name {  get; set; }
+        public string Email {  get; set; }
         public DateTime CreatedDate { get; set; }
         public string CommentText {  get; set; }
         public int BlogId {  get; set; }
