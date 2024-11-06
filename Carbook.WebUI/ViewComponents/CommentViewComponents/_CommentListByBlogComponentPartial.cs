5ff66546b699481ecca5b09e3dd7cb828851d28d
boratln <boratulan123@gmail.com>
2024-11-06 20:57:10 +0300

    Update _CommentListByBlogComponentPartial.cs

--- Carbook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Carbook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.blogid = id;
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Comments/CommentListByBlog/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
