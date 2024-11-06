ef3856931eb592ba756d205ebd12c2f87d971bae
boratln <boratulan123@gmail.com>
2024-11-06 20:57:42 +0300

    Create _SocialMediaUILayoutComponentPartial.cs

---
+++ Carbook.WebUI/ViewComponents/SocialMediaViewComponents/_SocialMediaUILayoutComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Carbook.Dto.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Carbook.WebUI.ViewComponents.SocialMediaViewComponents
+{
+    public class _SocialMediaUILayoutComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SocialMediaUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/SocialMedias");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
