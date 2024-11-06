42763df29ac70d4b9e30f85df2e0a783006e72dc
boratln <boratulan123@gmail.com>
2024-11-06 20:56:45 +0300

    Session Import

--- Carbook.WebUI/Program.cs
+++ Carbook.WebUI/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient();
+builder.Services.AddSession();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 var app = builder.Build();
@@ -14,6 +15,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
