fae0449301b98373e07669446a31414c5954c181
boratln <boratulan123@gmail.com>
2024-11-06 20:54:23 +0300

    Update ResultCommentDto.cs

--- Carbook.Dto/CommentDtos/ResultCommentDto.cs
+++ Carbook.Dto/CommentDtos/ResultCommentDto.cs
@@ -10,6 +10,8 @@ namespace Carbook.Dto.CommentDtos
     {
         public int CommentId { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+
         public DateTime CreatedDate { get; set; }
         public string CommentText { get; set; }
         public int BlogId { get; set; }
