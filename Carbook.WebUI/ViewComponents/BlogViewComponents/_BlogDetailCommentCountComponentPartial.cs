26a28bceb73ccffc38e645f09615d507cf9cafbc
boratln <boratulan123@gmail.com>
2024-11-06 20:57:05 +0300

    Web UI BlogDetailCommentCountComponentPartial

---
+++ Carbook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCommentCountComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogDetailCommentCountComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailCommentCountComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Comments/CommentCountByBlogId/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var count=JsonConvert.DeserializeObject<int>(jsonData);
+                ViewBag.count = count;
+                return View();
+            }
+            return View();
+        }
+    }
+}
