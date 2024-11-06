95ae93e1268d4e3c0124bc6e1a8ac679891700f2
boratln <boratulan123@gmail.com>
2024-11-06 20:57:40 +0300

    Update _FooterUILayoutComponentPartial.cs

--- Carbook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Carbook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult>InvokeAsync()
         {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var value=JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(value);
+            }
             return View();
         }
     }
