6cb696d0727cf023fabccd21763a5464c37f4c0d
boratln <boratulan123@gmail.com>
2024-11-07 20:28:33 +0300

    Create _AdminDashboardBlogListComponentPartial.cs

---
+++ Carbook.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Carbook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Carbook.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardBlogListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Blogs/GetAllBlogWithAuthor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
