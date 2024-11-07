e64e83287df63d493ee4d98c91509bac0adccb00
boratln <boratulan123@gmail.com>
2024-11-07 20:28:45 +0300

    Create _AdminScriptsComponentPartial.cs

---
+++ Carbook.WebUI/ViewComponents/DashboardViewComponents/_AdminScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminScriptsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
