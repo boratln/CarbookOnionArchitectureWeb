85690f43bf632bcdb7834a0a159a301e131735b0
boratln <boratulan123@gmail.com>
2024-11-07 20:25:05 +0300

    Create CreateCarFeatureCommandHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/CarFeatureHandlers/CreateCarFeatureCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Carbook.Application.Features.Mediator.Commands.CarFeatureCommands;
+using Carbook.Application.Interfaces.CarFeatureInterfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class CreateCarFeatureCommandHandler : IRequestHandler<CreateCarFeatureCommand>
+    {
+        private readonly ICarFeatureRepository _carFeatureRepository;
+
+        public CreateCarFeatureCommandHandler(ICarFeatureRepository carFeatureRepository)
+        {
+            _carFeatureRepository = carFeatureRepository;
+        }
+
+        public async Task Handle(CreateCarFeatureCommand request, CancellationToken cancellationToken)
+        {
+             _carFeatureRepository.CreateCarFeatureByCar(new CarFeature
+            {
+                Available = request.Available,
+                CarId = request.CarId,
+                FeatureId = request.FeatureId,
+            });
+        }
+    }
+}
