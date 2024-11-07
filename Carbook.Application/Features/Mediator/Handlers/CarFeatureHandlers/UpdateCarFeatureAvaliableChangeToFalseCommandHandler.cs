90a5f34c14fc59fd7b0c89b362515fa7d56f3330
boratln <boratulan123@gmail.com>
2024-11-07 20:25:10 +0300

    Create UpdateCarFeatureAvaliableChangeToFalseCommandHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/CarFeatureHandlers/UpdateCarFeatureAvaliableChangeToFalseCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Carbook.Application.Features.Mediator.Commands.CarFeatureCommands;
+using Carbook.Application.Interfaces.CarFeatureInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class UpdateCarFeatureAvaliableChangeToFalseCommandHandler : IRequestHandler<UpdateCarFeatureAvaliableChangeToFalseCommand>
+    {
+        private readonly ICarFeatureRepository _carFeatureRepository;
+
+        public UpdateCarFeatureAvaliableChangeToFalseCommandHandler(ICarFeatureRepository carFeatureRepository)
+        {
+            _carFeatureRepository = carFeatureRepository;
+        }
+
+        public async Task Handle(UpdateCarFeatureAvaliableChangeToFalseCommand request, CancellationToken cancellationToken)
+        {
+            _carFeatureRepository.ChangeCarFeatureAvaliableToFalse(request.Id);
+        }
+    }
+}
