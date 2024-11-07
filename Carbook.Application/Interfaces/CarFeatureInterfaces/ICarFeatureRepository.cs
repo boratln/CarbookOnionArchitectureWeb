72320b32aa3954b67d21ca3f3fcd16a09525335e
boratln <boratulan123@gmail.com>
2024-11-07 20:25:24 +0300

    Create ICarFeatureRepository.cs

---
+++ Carbook.Application/Interfaces/CarFeatureInterfaces/ICarFeatureRepository.cs
@@ -0,0 +1,17 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces.CarFeatureInterfaces
+{
+    public interface ICarFeatureRepository
+    {
+        List<CarFeature> GetCarFeatureByCarId(int carId);
+        void ChangeCarFeatureAvaliableToFalse(int id);
+        void ChangeCarFeatureAvaliableToTrue(int id);
+        void CreateCarFeatureByCar(CarFeature CarFeature);
+    }
+}
