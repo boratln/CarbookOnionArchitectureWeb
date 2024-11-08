983b84e982ade7ffae1eca46ecab0401e3257d68
boratln <boratulan123@gmail.com>
2024-11-08 22:21:44 +0300

    Create GetReviewCountQuery.cs

---
+++ Carbook.Application/Features/Mediator/Queries/ReviewQueries/GetReviewCountQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.ReviewResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.ReviewQueries
+{
+	public class GetReviewCountQuery:IRequest<GetReviewCountQueryResult>
+	{
+		public int Id {  get; set; }
+
+		public GetReviewCountQuery(int ıd)
+		{
+			Id = ıd;
+		}
+	}
+}
