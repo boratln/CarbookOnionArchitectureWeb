46c811fba418dc40c2cc1b44ff782b44d266311e
boratln <boratulan123@gmail.com>
2024-11-08 22:21:00 +0300

    Create CreateCarDescriptionCommand.cs

---
+++ Carbook.Application/Features/Mediator/Commands/CarDescriptionCommands/CreateCarDescriptionCommand.cs
@@ -0,0 +1,16 @@
+﻿using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Commands.CarDescriptionCommands
+{
+	public class CreateCarDescriptionCommand:IRequest
+	{
+		public int CarId { get; set; }
+		public string Details { get; set; }
+	}
+}
