8d191f10bdd1d479fee4f54d4aa17b620f3a5ded
boratln <boratulan123@gmail.com>
2024-11-08 22:27:22 +0300

    Create CarDescriptionRepository.cs

---
+++ Carbook.Persistence/Repositories/CarDescriptionRepositories/CarDescriptionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Application.Interfaces.CarDescriptionInterfaces;
+using Carbook.Domain.Entities;
+using Carbook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories.CarDescriptionRepositories
+{
+	public class CarDescriptionRepository : ICarDescriptionRepository
+	{
+		private readonly CarbookContext _context;
+
+		public CarDescriptionRepository(CarbookContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<CarDescription> GetCarDescriptionByCarId(int carId)
+		{
+			var value = await _context.CarDescriptions.Include(x => x.Car).Where(x => x.CarId == carId).FirstOrDefaultAsync();
+			return value;
+		}
+	}
+}
