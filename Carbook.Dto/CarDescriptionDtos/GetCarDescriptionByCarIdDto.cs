f1903f1273e1aa768f935c5644bb15a857b9380f
boratln <boratulan123@gmail.com>
2024-11-08 22:23:58 +0300

    Web UI GetCarDescriptionByCarIdDto

---
+++ Carbook.Dto/CarDescriptionDtos/GetCarDescriptionByCarIdDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Dto.CarDescriptionDtos
+{
+	public class GetCarDescriptionByCarIdDto
+	{
+		public int CarDescriptionId { get; set; }
+		public int CarId { get; set; }
+		public string Details { get; set; }
+	}
+}
