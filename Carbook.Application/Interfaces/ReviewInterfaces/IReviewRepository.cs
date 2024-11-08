1655e5848e8c3dfc5a4ecbdcede8ec447cc049ec
boratln <boratulan123@gmail.com>
2024-11-08 22:22:13 +0300

    Create IReviewRepository.cs

---
+++ Carbook.Application/Interfaces/ReviewInterfaces/IReviewRepository.cs
@@ -0,0 +1,15 @@
+﻿using Carbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Interfaces.ReviewInterfaces
+{
+	public interface IReviewRepository
+	{
+		Task<List<Review>> GetReviewsByCarId(int carId);
+		int  ReviewCount(int carId);
+	}
+}
