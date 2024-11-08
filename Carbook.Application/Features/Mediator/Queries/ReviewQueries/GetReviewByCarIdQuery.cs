6b56a61ccca4e28f314ea86b39ccd352dafb9801
boratln <boratulan123@gmail.com>
2024-11-08 22:21:41 +0300

    Create GetReviewByCarIdQuery.cs

---
+++ Carbook.Application/Features/Mediator/Queries/ReviewQueries/GetReviewByCarIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.ReviewResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.ReviewQueries
+{
+	public class GetReviewByCarIdQuery:IRequest<List<GetReviewByCarIdQueryResult>>
+	{
+		public int Id { get; set; }
+
+		public GetReviewByCarIdQuery(int ıd)
+		{
+			Id = ıd;
+		}
+	}
+}
