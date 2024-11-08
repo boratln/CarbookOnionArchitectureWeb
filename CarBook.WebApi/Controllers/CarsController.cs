e663f8f74a5072cc689b7d7483d215d33561daf8
boratln <boratulan123@gmail.com>
2024-11-08 22:28:24 +0300

    Update CarsController.cs

--- CarBook.WebApi/Controllers/CarsController.cs
+++ CarBook.WebApi/Controllers/CarsController.cs
@@ -19,26 +19,28 @@ namespace CarBook.WebApi.Controllers
         private readonly RemoveCarCommandHandler _removeCarCommandHandler;
         private readonly GetCarWithBrandQueryHandler _getCarWithBrandQueryHandler;
         private readonly GetLast5CarWithBrandQueryHandler? _getLast5CarWithBrandQueryHandler;
-        private readonly IMediator _mediator;
+        private readonly GetCarWithBrandByCarIdQueryHandler getCarWithBrandByCarIdQueryHandler;
+		private readonly IMediator _mediator;
 
 
 
-        public CarsController(CreateCarCommandHandler createCarCommandHandler, UpdateCarCommandHandler updateCarCommandHandler,
-            GetCarByIdQueryHandler getCarByIdQueryHandler, GetCarQueryHandler getCarQueryHandler, RemoveCarCommandHandler removeCarCommandHandler,
-            GetCarWithBrandQueryHandler getCarWithBrandQueryHandler, GetLast5CarWithBrandQueryHandler getLast5CarWithBrandQueryHandler
-, IMediator mediator)
-        {
-            _createCarCommandHandler = createCarCommandHandler;
-            _updateCarCommandHandler = updateCarCommandHandler;
-            _removeCarCommandHandler = removeCarCommandHandler;
-            _getCarByIdQueryHandler = getCarByIdQueryHandler;
-            _getCarQueryHandler = getCarQueryHandler;
-            _getCarWithBrandQueryHandler = getCarWithBrandQueryHandler;
-            _getLast5CarWithBrandQueryHandler = getLast5CarWithBrandQueryHandler;
-            _mediator = mediator;
-        }
+		public CarsController(CreateCarCommandHandler createCarCommandHandler, UpdateCarCommandHandler updateCarCommandHandler,
+			GetCarByIdQueryHandler getCarByIdQueryHandler, GetCarQueryHandler getCarQueryHandler, RemoveCarCommandHandler removeCarCommandHandler,
+			GetCarWithBrandQueryHandler getCarWithBrandQueryHandler, GetLast5CarWithBrandQueryHandler getLast5CarWithBrandQueryHandler
+, IMediator mediator, GetCarWithBrandByCarIdQueryHandler getCarWithBrandByCarIdQueryHandler)
+		{
+			_createCarCommandHandler = createCarCommandHandler;
+			_updateCarCommandHandler = updateCarCommandHandler;
+			_removeCarCommandHandler = removeCarCommandHandler;
+			_getCarByIdQueryHandler = getCarByIdQueryHandler;
+			_getCarQueryHandler = getCarQueryHandler;
+			_getCarWithBrandQueryHandler = getCarWithBrandQueryHandler;
+			_getLast5CarWithBrandQueryHandler = getLast5CarWithBrandQueryHandler;
+			_mediator = mediator;
+			this.getCarWithBrandByCarIdQueryHandler = getCarWithBrandByCarIdQueryHandler;
+		}
 
-        [HttpGet]
+		[HttpGet]
         public async Task<IActionResult> Get()
         {
             var values = await _getCarQueryHandler.Handle();
@@ -80,8 +82,16 @@ namespace CarBook.WebApi.Controllers
             await _removeCarCommandHandler.Handle(new RemoveCarCommand(id));
             return Ok("Araç başarıyla silindi");
         }
-       
-      
+        [HttpGet("GetCarWithBrand/{id}")]
+        public async Task<IActionResult> GetCarWithBrand(int id)
+        {
+            var value = await getCarWithBrandByCarIdQueryHandler.Handle(new GetCarWithBrandByCarIdQuery(id));
+            return Ok(value);
+
+		}
+
+
+
 
 	}
 }
