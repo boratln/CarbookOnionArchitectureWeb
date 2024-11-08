d9d3294264f2db30752663ff22c27b09bc964e9e
boratln <boratulan123@gmail.com>
2024-11-08 22:29:30 +0300

    Update CarPricingController.cs

--- Carbook.WebUI/Controllers/CarPricingController.cs
+++ Carbook.WebUI/Controllers/CarPricingController.cs
@@ -15,7 +15,9 @@ namespace Carbook.WebUI.Controllers
 
 		public async  Task<IActionResult> Index()
         {
-            TempData["title"] = "Paketler";
+			TempData["Navbar"] = "Ödeme Türleri";
+
+			TempData["title"] = "Paketler";
             TempData["desc"] = "Araç Fiyat Paketleri";
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7076/api/CarsPricings/GetCarPricingWithTimePeriodList");
