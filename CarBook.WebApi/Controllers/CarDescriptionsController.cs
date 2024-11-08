858c57e8c0d5d2de8a80d4a898af90c3ed865b49
boratln <boratulan123@gmail.com>
2024-11-08 22:28:21 +0300

    Web Api Car Descriptions Controller

---
+++ CarBook.WebApi/Controllers/CarDescriptionsController.cs
@@ -0,0 +1,33 @@
+﻿using Carbook.Application.Features.Mediator.Commands.CarDescriptionCommands;
+using Carbook.Application.Features.Mediator.Queries.CarDescriptionQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CarDescriptionsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CarDescriptionsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet("GetCarDescriptionByCarId/{id}")]
+		public async Task<IActionResult> GetCarDescriptionByCarId(int id)
+		{
+			var value = await _mediator.Send(new GetCarDescriptionByCarIdQuery(id));
+			return Ok(value);
+		}
+		[HttpPost("CreateCarDescription")]
+		public async Task<IActionResult> CreateCarDescription(CreateCarDescriptionCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Araba Açıklaması başarıyla eklendi");
+		}
+	}
+}
