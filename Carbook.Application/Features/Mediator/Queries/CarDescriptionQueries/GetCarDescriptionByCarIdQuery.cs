4a6d7d47aa84fd3a1347986697370b923c209953
boratln <boratulan123@gmail.com>
2024-11-08 22:21:25 +0300

    Create GetCarDescriptionByCarIdQuery.cs

---
+++ Carbook.Application/Features/Mediator/Queries/CarDescriptionQueries/GetCarDescriptionByCarIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Carbook.Application.Features.Mediator.Results.CarDescriptionResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Queries.CarDescriptionQueries
+{
+    public class GetCarDescriptionByCarIdQuery : IRequest<GetCarDescriptionByCarIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetCarDescriptionByCarIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
