840e06370f79727d3db58d57a047b2f838fac848
boratln <boratulan123@gmail.com>
2024-11-08 22:28:34 +0300

    Web Api Reviews Controller

---
+++ CarBook.WebApi/Controllers/ReviewsController.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.ReviewQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ReviewsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public ReviewsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		[HttpGet("GetReviewsByCarId/{id}")]
+		public async Task<IActionResult> GetReviewsByCarId(int id)
+		{
+			var values = await _mediator.Send(new GetReviewByCarIdQuery(id));
+			return Ok(values);
+		}
+		[HttpGet("GetReviewCountByCarId/{id}")]
+		public async Task<IActionResult> GetReviewCountByCarId(int id)
+		{
+			var count=await _mediator.Send(new GetReviewCountQuery(id));
+			return Ok(count);
+		}
+	}
+}
