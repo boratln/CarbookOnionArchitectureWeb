5173e7b99670cbd06f1f20fc3ba96a9ad9bd11e4
boratln <boratulan123@gmail.com>
2024-11-08 22:30:12 +0300

    Create _CarrDetailCarDescriptionComponentPartial.cs

---
+++ Carbook.WebUI/ViewComponents/CarDetailViewComponents/_CarrDetailCarDescriptionComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Carbook.Dto.CarDescriptionDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarrDetailCarDescriptionComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarrDetailCarDescriptionComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task <IViewComponentResult> InvokeAsync(int id)
+		{
+			var client=_httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7076/api/CarDescriptions/GetCarDescriptionByCarId/"+id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData=await responseMessage.Content.ReadAsStringAsync();
+				var value=JsonConvert.DeserializeObject<GetCarDescriptionByCarIdDto>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+	}
+}
