85f46580f22e85f81caa4d8c9b39193cd982e89e
boratln <boratulan123@gmail.com>
2024-11-08 22:23:28 +0300

    Review Entity

---
+++ Carbook.Domain/Entities/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Domain.Entities
+{
+	public class Review
+	{
+		public int ReviewId {  get; set; }
+		public string CustomerName {  get; set; }
+		public string CustomerImageUrl {  get; set; }
+		public string Comment {  get; set; }
+		public string Rating { get; set; }
+		public DateTime Rating_Date {  get; set; }
+		public int CarId {  get; set; }
+		public Car Car { get; set; }
+	}
+}
