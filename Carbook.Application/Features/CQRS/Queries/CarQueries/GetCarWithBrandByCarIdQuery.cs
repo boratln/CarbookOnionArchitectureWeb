0ddc299f826d9ebbacbe70e5d275316f28654109
boratln <boratulan123@gmail.com>
2024-11-08 22:20:36 +0300

    GetCarWithBrandByCarIdQuery

---
+++ Carbook.Application/Features/CQRS/Queries/CarQueries/GetCarWithBrandByCarIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.CQRS.Queries.CarQueries
+{
+	public class GetCarWithBrandByCarIdQuery
+	{
+        public int Id { get; set; }
+
+		public GetCarWithBrandByCarIdQuery(int ıd)
+		{
+			Id = ıd;
+		}
+	}
+}
