9f641701ae335df1e785f61b36794a17c87c4e8d
boratln <boratulan123@gmail.com>
2024-11-08 22:21:06 +0300

    Create CreateCarDescriptionCommandHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/CarDescriptionHandlers/CreateCarDescriptionCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Commands.CarDescriptionCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.CarDescriptionHandlers
+{
+	public class CreateCarDescriptionCommandHandler : IRequestHandler<CreateCarDescriptionCommand>
+	{
+		private readonly IRepository<CarDescription> _repository;
+
+		public CreateCarDescriptionCommandHandler(IRepository<CarDescription> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateCarDescriptionCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.Create(new CarDescription
+			{
+				CarId= request.CarId,
+				Details= request.Details
+			});
+		}
+	}
+}
