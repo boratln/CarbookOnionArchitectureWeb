b0d60f8c9c9b6b51702c542444929cfd1cbdf282
boratln <boratulan123@gmail.com>
2024-11-08 22:21:20 +0300

    Create GetReviewCountQueryHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/ReviewHandlers/GetReviewCountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Application.Features.Mediator.Queries.ReviewQueries;
+using Carbook.Application.Features.Mediator.Results.ReviewResults;
+using Carbook.Application.Interfaces.ReviewInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+	public class GetReviewCountQueryHandler : IRequestHandler<GetReviewCountQuery, GetReviewCountQueryResult>
+	{
+		private readonly IReviewRepository _reviewRepository;
+
+		public GetReviewCountQueryHandler(IReviewRepository reviewRepository)
+		{
+			_reviewRepository = reviewRepository;
+		}
+
+		public async Task<GetReviewCountQueryResult> Handle(GetReviewCountQuery request, CancellationToken cancellationToken)
+		{
+			var count =  _reviewRepository.ReviewCount(request.Id);
+			return new GetReviewCountQueryResult
+			{
+				ReviewCount = count
+			};
+		}
+	}
+}
