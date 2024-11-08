2bb8d55f85cee2a764f7533ecc4f01608a1493ca
boratln <boratulan123@gmail.com>
2024-11-08 22:21:04 +0300

    Create RemoveCarDescriptionCommand.cs

---
+++ Carbook.Application/Features/Mediator/Commands/CarDescriptionCommands/RemoveCarDescriptionCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Commands.CarDescriptionCommands
+{
+	public class RemoveCarDescriptionCommand:IRequest
+	{
+		public int Id {  get; set; }
+
+		public RemoveCarDescriptionCommand(int ıd)
+		{
+			Id = ıd;
+		}
+	}
+}
