a34f35870a69f18f337dc7658bf5d683f4c4bcac
boratln <boratulan123@gmail.com>
2024-11-09 18:15:03 +0300

    JWT Token Defaults

---
+++ Carbook.Application/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Tools
+{
+	public class JwtTokenDefaults
+	{
+		public const string ValidAudience = "https://localhost";
+		public const string ValidIssiuer = "http://localhost";
+		public const string Key = "Carbook01Carbook014548+CarbookProje01214+";
+		public const int Expire = 3;
+	}
+}
