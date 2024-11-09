e5baa4cf019cca1f68235634442f988f8117819f
boratln <boratulan123@gmail.com>
2024-11-09 19:13:06 +0300

    Update AdminCategoryController.cs

--- Carbook.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
+++ Carbook.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -20,7 +20,7 @@ namespace Carbook.WebUI.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7076/api/Categorys");
+            var responseMessage = await client.GetAsync("https://localhost:7076/api/Categories");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -43,7 +43,7 @@ namespace Carbook.WebUI.Areas.Admin.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(createCategoryDto);
             var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:7076/api/Categorys", content);
+            var responseMessage = await client.PostAsync("https://localhost:7076/api/Categories", content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
@@ -56,7 +56,7 @@ namespace Carbook.WebUI.Areas.Admin.Controllers
         public async Task<IActionResult> DeleteCategory(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Categorys/{id}");
+            var responseMessage = await client.DeleteAsync($"https://localhost:7076/api/Categories/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "AdminCategory", new { area = "Admin" });
@@ -72,7 +72,7 @@ namespace Carbook.WebUI.Areas.Admin.Controllers
         {
             HttpContext.Session.SetInt32("Categoryid", id);
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Categorys/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7076/api/Categories/{id}");
 
             if (responseMessage.IsSuccessStatusCode)
             {
@@ -93,7 +93,7 @@ namespace Carbook.WebUI.Areas.Admin.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
             var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("https://localhost:7076/api/Categorys", content);
+            var responseMessage = await client.PutAsync("https://localhost:7076/api/Categories", content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 HttpContext.Session.Remove("Categoryid");
