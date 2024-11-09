58ac350d7c150fa9eb49cece2c74b2bd54f0b7ee
boratln <boratulan123@gmail.com>
2024-11-09 18:17:01 +0300

    Update Repository.cs

--- Carbook.Persistence/Repositories/Repository.cs
+++ Carbook.Persistence/Repositories/Repository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,7 +29,12 @@ namespace Carbook.Persistence.Repositories
             return await _context.Set<T>().ToListAsync();
         }
 
-        public async Task<T> GetById(int id)
+		public async Task<T?> GetByFilter(Expression<Func<T, bool>> filter)
+		{
+			return await _context.Set<T>().SingleOrDefaultAsync(filter);
+		}
+
+		public async Task<T> GetById(int id)
         {
             return await _context.Set<T>().FindAsync(id);
         }
