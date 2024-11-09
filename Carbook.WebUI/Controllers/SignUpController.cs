f1bffe22f6f4034e8b277f7e1a6cd0f6a9096aae
boratln <boratulan123@gmail.com>
2024-11-09 18:18:38 +0300

    Create SignUpController.cs

---
+++ Carbook.WebUI/Controllers/SignUpController.cs
@@ -0,0 +1,36 @@
+﻿using Carbook.Dto.BrandDtos;
+using Carbook.Dto.SignUpDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Carbook.WebUI.Controllers
+{
+	public class SignUpController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public SignUpController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		[HttpGet]
+		public IActionResult CreateAppUser()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async  Task<IActionResult> CreateAppUser(CreateSignUpDto createSignUpDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createSignUpDto);
+			var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7076/api/SignUp", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Default");
+			}
+			return RedirectToAction("Index", "Default");
+		}
+	}
+}
