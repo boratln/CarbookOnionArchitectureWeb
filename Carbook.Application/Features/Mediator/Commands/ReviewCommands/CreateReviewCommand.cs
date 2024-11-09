e7c7f9076d14f1edca1cc37462b559a4a25129a7
boratln <boratulan123@gmail.com>
2024-11-09 18:14:13 +0300

    Create CreateReviewCommand.cs

---
+++ Carbook.Application/Features/Mediator/Commands/ReviewCommands/CreateReviewCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Commands.ReviewCommands
+{
+	public class CreateReviewCommand:IRequest
+	{
+		public string CustomerName { get; set; }
+		public string CustomerImageUrl { get; set; }
+		public string Comment { get; set; }
+		public string Rating { get; set; }
+		public DateTime Rating_Date { get; set; }
+		public int CarId { get; set; }
+	}
+}
