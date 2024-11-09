d81d9f94328c1e4f9fa64d94b29624e416d053af
boratln <boratulan123@gmail.com>
2024-11-09 18:14:03 +0300

    Token Response Dto

---
+++ Carbook.Application/Dtos/TokenResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Dtos
+{
+	public class TokenResponseDto
+	{
+		public TokenResponseDto(string token,DateTime expireDate) {
+		Token= token;
+			Expire_Date = expireDate;
+		}
+		public string Token { get; set; }
+		public DateTime Expire_Date {  get; set; }
+	}
+}
