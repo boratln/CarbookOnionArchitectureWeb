6c2404c708217f87533d90f6a3610d2bbb006f70
boratln <boratulan123@gmail.com>
2024-11-09 18:14:23 +0300

    Create CreateReviewCommandHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/ReviewHandlers/CreateReviewCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Carbook.Application.Features.Mediator.Commands.ReviewCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+	public class CreateReviewCommandHandler:IRequestHandler<CreateReviewCommand>
+	{
+		private readonly IRepository<Review> _repository;
+
+		public CreateReviewCommandHandler(IRepository<Review> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.Create(new Review
+			{
+				CarId = request.CarId,
+				Comment = request.Comment,
+				CustomerImageUrl = request.CustomerImageUrl,
+				CustomerName = request.CustomerName,
+				Rating = request.Rating,
+				Rating_Date = DateTime.Parse(DateTime.Now.ToShortDateString())
+				
+			
+			});
+		}
+	}
+}
