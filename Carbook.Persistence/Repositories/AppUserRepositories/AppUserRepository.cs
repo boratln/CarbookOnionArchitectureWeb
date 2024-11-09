72a20bc9f8ca97f9c3c0b94dfbc1c737954764b5
boratln <boratulan123@gmail.com>
2024-11-09 18:16:56 +0300

    Create AppUserRepository.cs

---
+++ Carbook.Persistence/Repositories/AppUserRepositories/AppUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Carbook.Application.Interfaces.AppUserInterfaces;
+using Carbook.Domain.Entities;
+using Carbook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories.AppUserRepositories
+{
+	public class AppUserRepository : IAppUserRepository
+	{
+		private readonly CarbookContext _carbookContext;
+
+		public AppUserRepository(CarbookContext carbookContext)
+		{
+			_carbookContext = carbookContext;
+		}
+
+		public async Task<List<AppUser>> GetByFilter(Expression<Func<AppUser, bool>> filter)
+		{
+			var values = await _carbookContext.AppUsers.Where(filter).ToListAsync();
+
+			return values;
+		}
+	}
+}
