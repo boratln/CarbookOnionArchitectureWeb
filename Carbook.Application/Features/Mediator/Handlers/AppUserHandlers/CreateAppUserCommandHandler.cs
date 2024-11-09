d0a0562b13006c422c1651996071dbb4a8abfecd
boratln <boratulan123@gmail.com>
2024-11-09 18:14:18 +0300

    Create CreateAppUserCommandHandler.cs

---
+++ Carbook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Carbook.Application.Enums;
+using Carbook.Application.Features.Mediator.Commands.AppUserCommands;
+using Carbook.Application.Interfaces;
+using Carbook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+	public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+	{
+		private readonly IRepository<AppUser> _appUserRepository;
+
+		public CreateAppUserCommandHandler(IRepository<AppUser> appUserRepository)
+		{
+			_appUserRepository = appUserRepository;
+		}
+
+		public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+		{
+			await _appUserRepository.Create(new AppUser
+			{
+				Username = request.Username,
+				Password = request.Password,
+				AppRoleId=(int) RolesType.Member,
+				Email = request.Email,
+				Name = request.Name,
+				Surname = request.Surname,
+				
+			});
+		}
+	}
+}
