c2ee121bcd8711b4ff8b6051085b8ab59fe58bc0
boratln <boratulan123@gmail.com>
2024-11-09 18:17:45 +0300

    Create SignUpController.cs

---
+++ CarBook.WebApi/Controllers/SignUpController.cs
@@ -0,0 +1,24 @@
+﻿using Carbook.Application.Features.Mediator.Commands.AppUserCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SignUpController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public SignUpController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateUser(CreateAppUserCommand command) {
+			await _mediator.Send(command);
+			return Ok("Kullanıcı başarıyla oluşturuldu");
+		}
+	}
+}
