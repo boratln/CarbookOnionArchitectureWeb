f9a87ea51f750a28f477bf75e80f5be4b1b36237
boratln <boratulan123@gmail.com>
2024-11-09 18:14:44 +0300

    Get By Filter Method

--- Carbook.Application/Interfaces/IRepository.cs
+++ Carbook.Application/Interfaces/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace Carbook.Application.Interfaces
         Task Create(T entity);
         Task Update(T entity);
         Task Remove(T entity);
+        Task <T?> GetByFilter(Expression<Func<T, bool>> filter);
     }
 }
